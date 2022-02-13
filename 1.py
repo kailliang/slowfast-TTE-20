1cd26b0253488829263a924818499ba2c451a7bb
Kai <56094206+kailliang@users.noreply.github.com>
2022-02-13 22:42:42 +0000

    Update and rename 2.py to 1.py

--- 1.py
+++ 1.py
@@ -67,14 +67,10 @@ class PackPathway(nn.Module):
 
 
 train_transform = ApplyTransformToKey(key="video", transform=Compose(
-    [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std), RandomRotation([-5,5]), 
-     RandomShortSideScale(min_size=side_size, max_size=max_size), CenterCropVideo(crop_size), 
-     ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5), PackPathway()]))
+    [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std), ShortSideScale(size=side_size), PackPathway()]))
 
 test_transform = ApplyTransformToKey(key="video", transform=Compose(
-    [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std), RandomRotation([-5,5]), 
-     RandomShortSideScale(min_size=side_size, max_size=max_size), CenterCropVideo(crop_size), 
-     ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5), PackPathway()]))
+    [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std), ShortSideScale(size=side_size), PackPathway()]))
 
 
 
